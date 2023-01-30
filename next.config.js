a37fa7b963a38c88e142d2f7cac89aea0275e147
fdelgado8617 <f.delgado@ecrchs.net>
2023-01-29 20:45:34 -0800

    change call order to match

--- next.config.js
+++ next.config.js
@@ -5,10 +5,10 @@ const nextConfig = {
 
 module.exports = {
   env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     AZURE_AD_CLIENT_ID: process.env.AZURE_AD_CLIENT_ID,
     AZURE_AD_CLIENT_SECRET: process.env.AZURE_AD_CLIENT_SECRET,
     AZURE_AD_TENANT_ID: process.env.AZURE_AD_TENANT_ID,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL
   }
 };
