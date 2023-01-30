4dc267e4b9ab385a1a5f7c04f479d977f1ad6355
fdelgado8617 <f.delgado@ecrchs.net>
2023-01-29 21:15:15 -0800

    update call back uri

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -19,7 +19,7 @@ export default NextAuth({
   callbacks: {
     async redirect({ url, baseUrl }) {
       // Allows relative callback URLs
-      if (url.startsWith("/")) return new URL(url, baseUrl).toString()
+      if (url.startsWith("/")) return `${baseUrl}${url}`
       // Allows callback URLs on the same origin
       else if (new URL(url).origin === baseUrl) return url
       return url
