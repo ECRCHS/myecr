f0598eaa0e7ff79502db4e3ecb00c903fe875c91
fdelgado8617 <f.delgado@ecrchs.net>
2023-01-29 21:18:01 -0800

    update module exports

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
 }
 
 module.exports = {
+  reactStrictMode: true,
   env: {
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     AZURE_AD_CLIENT_ID: process.env.AZURE_AD_CLIENT_ID,
